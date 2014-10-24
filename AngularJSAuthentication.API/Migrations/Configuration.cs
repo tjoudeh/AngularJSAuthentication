9e69f0071db36d7f1de97b1cd96da133f0363049
Dmitry <dmitry.parfenchik@gmail.com>
2014-10-24 12:23:02 +0200

    Making clients updatable via Seed method in Entity migrations

--- AngularJSAuthentication.API/Migrations/Configuration.cs
+++ AngularJSAuthentication.API/Migrations/Configuration.cs
@@ -1,11 +1,7 @@
 namespace AngularJSAuthentication.API.Migrations
 {
     using AngularJSAuthentication.API.Entities;
-    using System;
-    using System.Collections.Generic;
-    using System.Data.Entity;
     using System.Data.Entity.Migrations;
-    using System.Linq;
 
     internal sealed class Configuration : DbMigrationsConfiguration<AngularJSAuthentication.API.AuthContext>
     {
@@ -16,41 +12,27 @@ namespace AngularJSAuthentication.API.Migrations
 
         protected override void Seed(AngularJSAuthentication.API.AuthContext context)
         {
-            if (context.Clients.Count() > 0)
+            context.Clients.AddOrUpdate(t => t.Id, new Client
             {
-                return;
-            }
-
-            context.Clients.AddRange(BuildClientsList());
-            context.SaveChanges();
-        }
-
-        private static List<Client> BuildClientsList()
-        {
-
-            List<Client> ClientsList = new List<Client> 
-            {
+                Id = "ngAuthApp",
+                Secret = Helper.GetHash("abc@123"),
+                Name = "AngularJS front-end Application",
+                ApplicationType = Models.ApplicationTypes.JavaScript,
+                Active = true,
+                RefreshTokenLifeTime = 7200,
+                AllowedOrigin = "http://localhost:32150"
+            },
                 new Client
-                { Id = "ngAuthApp", 
-                    Secret= Helper.GetHash("abc@123"), 
-                    Name="AngularJS front-end Application", 
-                    ApplicationType =  Models.ApplicationTypes.JavaScript, 
-                    Active = true, 
-                    RefreshTokenLifeTime = 7200, 
-                    AllowedOrigin = "http://localhost:26264/"
-                },
-                new Client
-                { Id = "consoleApp", 
-                    Secret=Helper.GetHash("123@abc"), 
-                    Name="Console Application", 
-                    ApplicationType =Models.ApplicationTypes.NativeConfidential, 
-                    Active = true, 
-                    RefreshTokenLifeTime = 14400, 
+                {
+                    Id = "consoleApp",
+                    Secret = Helper.GetHash("123@abc"),
+                    Name = "Console Application",
+                    ApplicationType = Models.ApplicationTypes.NativeConfidential,
+                    Active = true,
+                    RefreshTokenLifeTime = 14400,
                     AllowedOrigin = "*"
-                }
-            };
-
-            return ClientsList;
+                });
+            context.SaveChanges();
         }
     }
 }
